6a9f0990b402aecf80795df51313bda98b908eb2
Nickaltstein@gmail.com <nickaltstein@gmail.com>
2026-01-12 22:04:22 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    background: './src/background.js',
+    content_script: './src/content_script.js',
+    popup: './src/popup.js',
+    'sidebar-app': './src/sidebar/index.jsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: 'manifest.json' },
+        { from: 'public/icons', to: 'icons' },
+        { from: 'public/*.html', to: '[name][ext]' }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
